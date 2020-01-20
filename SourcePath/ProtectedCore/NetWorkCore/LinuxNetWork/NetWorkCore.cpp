455b54baaeb5df90db59298fcebaa07bc32f9432
ShawnLee <54459770@qq.com>
2020-01-20 11:30:52 +0800

    Linux network core commit

---
+++ SourcePath/ProtectedCore/NetWorkCore/LinuxNetWork/NetWorkCore.cpp
@@ -0,0 +1,134 @@
+
+#include "../NetWorkCore.h"
+#include "../../../CoreInterface/IModuleInterface.h"
+#include "../../../PublicLib/Include/Common/tools.h"
+#include "../../../PublicLib/Include/Common/TypeDefines.h"
+#include <vector>
+#include <string>
+
+SL_NetWorkCore::SL_NetWorkCore()
+{}
+SL_NetWorkCore::~SL_NetWorkCore()
+{}
+
+#pragma region Interface
+//	初始化函数
+bool SL_NetWorkCore::Initialize(IModule* pModule)
+{
+#pragma region Module Core Necessary
+	if (nullptr == pModule)
+		return false;
+
+	m_pModule = pModule;
+
+	ISystemCore* pSystemCore = pModule->GetSystemCore();
+	if (nullptr == pSystemCore)
+		return false;
+
+	m_pSysCore = pSystemCore;
+#pragma endregion
+
+	bool bRet = IsEnable();
+
+
+	return bRet;
+}
+
+bool SL_NetWorkCore::Destroy()
+{
+	return true;
+}
+
+#pragma region socket about
+
+#pragma region listen socket
+//	create listen socket
+bool SL_NetWorkCore::CreateListenSocket(const char* strAddress, const char* strPort)
+{
+	if (!CheckStringValid(strAddress))
+		return false;
+
+	if (!CheckStringValid(strPort))
+		return false;
+
+	SI32 nPort = atoi(strPort);
+	if (0 >= nPort)
+		return false;
+
+	return CreateListenSocket(strAddress, nPort);
+}
+
+//	create listen socket
+bool SL_NetWorkCore::CreateListenSocket(const char* strAddress, int nPort)
+{
+	if (!CheckStringValid(strAddress))
+		return false;
+
+	if (0 >= nPort)
+		return false;
+
+	return true;
+}
+
+//	create listen socket("xx.xx.xx.xx:xxxx")
+bool SL_NetWorkCore::CreateListenSocket(const char* strAddress)
+{
+	if (!CheckStringValid(strAddress))
+		return false;
+
+	std::vector<std::string> vAddr = SplitString(strAddress, ":");
+	if (vAddr.size() != 2)
+		return false;
+
+	return CreateListenSocket(vAddr[0].c_str(), vAddr[1].c_str());
+}
+#pragma endregion 
+
+#pragma region connect socket
+//	create connect socket
+bool SL_NetWorkCore::CreateConnectSocket(const char* strAddress, const char* strPort)
+{
+	if (!CheckStringValid(strAddress))
+		return false;
+
+	if (!CheckStringValid(strPort))
+		return false;
+
+	SI32 nPort = atoi(strPort);
+	if (0 >= nPort)
+		return false;
+
+	return CreateConnectSocket(strAddress, nPort);
+}
+
+//	create connect socket
+bool SL_NetWorkCore::CreateConnectSocket(const char* strAddress, int nPort)
+{
+	if (!CheckStringValid(strAddress))
+		return false;
+
+	if (0 >= nPort)
+		return false;
+
+	return true;
+}
+
+//	create connect socket
+bool SL_NetWorkCore::CreateConnectSocket(const char* strAddress)
+{
+	if (!CheckStringValid(strAddress))
+		return false;
+
+	std::vector<std::string> vAddr = SplitString(strAddress, ":");
+	if (vAddr.size() != 2)
+		return false;
+
+	return CreateConnectSocket(vAddr[0].c_str(), vAddr[1].c_str());
+}
+#pragma endregion
+#pragma endregion
+
+#pragma endregion
+
+
+
