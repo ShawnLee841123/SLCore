45a9861027211956982ddf3c064ffb67a2b09bc1
ShawnLee <54459770@qq.com>
2020-01-23 16:31:01 +0800

    Add Common Linux compile command

--- SourcePath/Common.cmake
+++ SourcePath/Common.cmake
@@ -5,7 +5,9 @@ SET(LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR})
 
 if(WIN32)
 else(WIN32)
-add_definitions(-fPIC -std=c++11)
+	add_definitions(-fPIC -std=c++11)
+	add_definitions(-DNDEBUG)
+	add_definitions(-g -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free)
 endif(WIN32)
 
 
