20bf966a527d7f8f5b6d5a7533a62f9ba25722c8
Shawn Lee <54459770@qq.com>
2020-01-23 13:28:52 +0800

    Modify Linux Initialize shell script file

--- DevOps/Scripts/Linux_SOFileInitialize.sh
+++ DevOps/Scripts/Linux_SOFileInitialize.sh
@@ -3,4 +3,4 @@ echo "123456"|sudo sh -c "echo 'pwd'../Modules >> /etc/ld.so.conf.d/SLCore.conf"
 
 echo "123456"|sudo ldconfig
 
-
+echo "123456"|sudo sh -c "echo /home/shawnlee/GitWork/SLCore/SLCore/DevOps/DumpPath/core.%e.%p.%t > /proc/sys/kernel/core_pattern"
