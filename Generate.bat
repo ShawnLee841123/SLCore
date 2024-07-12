a796b53542eca67021ba6bf26ba6201a41fdc0ac
ShanwLee <54459770@qq.com>
2024-07-12 16:31:42 +0800

    Modificate generate vs project script for vs2022

--- Generate.bat
+++ Generate.bat
@@ -5,7 +5,7 @@
 ::@set VSCLDir=VS_Client
 @set VSCCDir=Build
 
-@set ProRoot=G:/Works/MyGit/SLCore/
+@set ProRoot=%CD%
 
 ::md %VSCLDir%
 ::cd %VSCLDir%
@@ -16,7 +16,8 @@
 
 md %VSCCDir%
 cd %VSCCDir%
-cmake -G"Visual Studio 15 2017 Win64" %ProRoot%/%CenterCore% -DBINARY_DIR=%ProRoot%/%VSCCDir% -DCMAKE_CONFIGURATION_TYPES=Debug
+::cmake -G"Visual Studio 15 2017 Win64" %ProRoot%/%CenterCore% -DBINARY_DIR=%ProRoot%/%VSCCDir% -DCMAKE_CONFIGURATION_TYPES=Debug
+cmake -G"Visual Studio 17 2022" A x64 %ProRoot%/%CenterCore% -DBINARY_DIR=%ProRoot%/%VSCCDir% -DCMAKE_CONFIGURATION_TYPES=Debug
 
 @echo ---------------------generate server project completed---------------------
 cd ..
